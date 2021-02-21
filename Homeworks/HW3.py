de075370fbb05597464c15ffc0b289d9789669c1
devSargeras <43101550+devSargeras@users.noreply.github.com>
2021-02-21 19:51:46 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,27 @@
-#Explain your work
+def prime_first(sayi):
+    if sayi > 1:
+        kontrol = True
+        for i in range(2, sayi):
+            if sayi % i == 0:
+                kontrol = False
+                break
+        if kontrol == True:
+            print(sayi)
 
-#Question 1
-for x in range(a):
-	print(a)
+
+def prime_second(sayi):
+    if sayi > 1:
+        kontrol = True
+        for i in range(2, sayi):
+            if sayi % i == 0:
+                kontrol = False
+                break
+        if kontrol == True:
+            print(sayi)
+
+
+for i in range(0, 1000):
+    if i < 500:
+        prime_first(i)
+    else:
+        prime_second(i)
